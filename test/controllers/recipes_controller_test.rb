44f0dd50e64dbcfa9d4f8f2d3f2a606d4fb456c3
jeffgrunewald <jeff@grunewalddesign.com>
2016-03-01 15:28:27 -0500

    added search form

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,10 +1,35 @@
 require 'test_helper'
 
 class RecipesControllerTest < ActionController::TestCase
-  test "should get index" do
+  def setup
     get :index
+  end
+
+  test 'should get index' do
     assert_response :success
     assert_response 200
+  end
+
+  test 'results should have three columns' do
+    assert_select 'th', 3
+  end
+
+  test 'page should display chocolate initially' do
+    assert_select 'h2', 'Searching for - chocolate'
+  end
+
+  test 'page should display bacon on subsequent search' do
+    get(:index, {'search' => 'bacon'})
+    assert_select 'h2', 'Searching for - bacon'
+  end
+
+  test 'page results should be zebra-striped' do
+    assert_select 'tr.even', 15
+    assert_select 'tr.odd', 15
+  end
+
+  test 'page gives credit to data source' do
+    assert_select 'p', 'Powered By Food2Fork.com'
   end
 
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
-    test "returns relevant results" do
+    test 'returns relevant results' do
         recipes = Recipe.for('apple')
         assert_not_nil recipes
         assert_equal recipes[1]['title'], 'Apple Dumplings'
+    end
+
+    test 'returns eight datapoints per recipe' do
+        recipes = Recipe.for('bacon')
+        assert_equal recipes[1].length, 8
+        assert_equal recipes[1].length, recipes[5].length
     end
 end
